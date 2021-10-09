79556d37e11d28b050572d402bc9dfb57b1cc351
Ricardo Milan <ricardo@intercambiodireto.com>
2021-10-09 18:03:25 -0300

    adicionando router

---
+++ src/Pages/About/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const About: React.FC = () => {
+  return <>About</>
+}
+
+export default About;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './Pages/About';
+import Home from './Pages/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>React App</h1>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
