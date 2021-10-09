aa94f70ca077610012644a03b01a2797a5492f8f
Ricardo Milan <ricardo@intercambiodireto.com>
2021-10-09 18:30:32 -0300

    menu responsivo

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -31,16 +31,54 @@ const App: React.FC = () => {
               </div>
             </div>
           </div>
+
+          <div className="md:hidden" id="mobile-menu">
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <Link
+                className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                to="/"
+              >
+                Home
+              </Link>
+
+              <Link
+                className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                to="/about"
+              >
+                About
+              </Link>
+            </div>
+          </div>
         </nav>
 
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <header className="bg-white shadow">
+          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-gray-900">
+              Teste Router/Axios/Tailwind
+            </h1>
+          </div>
+        </header>
+
+        <main>
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <Switch>
+              <Route path="/about">
+                <div className="px-4 py-6 sm:px-0">
+                  <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
+                    <About />
+                  </div>
+                </div>
+              </Route>
+              <Route path="/">
+                <div className="px-4 py-6 sm:px-0">
+                  <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
+                    <Home />
+                  </div>
+                </div>
+              </Route>
+            </Switch>
+          </div>
+        </main>
       </div>
     </Router>
   );
