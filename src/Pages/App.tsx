1b2bb485b503edd3ee0e22036d66cffc83380e3b
Ricardo Milan <ricardo@intercambiodireto.com>
2021-10-09 19:24:36 -0300

    texto header

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -54,7 +54,7 @@ const App: React.FC = () => {
         <header className="bg-white shadow">
           <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold text-gray-900">
-              Exemplo Router/Axios+React-Query/Tailwind
+              Exemplo Router / Axios / ReactQuery / Tailwind
             </h1>
           </div>
         </header>
